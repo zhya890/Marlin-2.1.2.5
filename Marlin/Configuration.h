a20436c5d008984560f2dbcf4f8d7ed6378cb49b
zhya890 <keiiisuke@gmail.com>
2025-06-18 19:35:56 +0900

    M503の値をなるべく書き込む｡ ほっとエンドとベッドのPID値､Zプローブのオフセット値､Input Shapingの周波数

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1513,7 +1513,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -51.00, -10.00, -1.68 }
+#define NOZZLE_TO_PROBE_OFFSET { -51.00, -10.00, -2.05 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
